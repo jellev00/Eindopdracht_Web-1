d4614c1d1cb6f10e89a6cc135519869f67cf2822
jellev00 <jelle.vandriessche@gmail.com>
2023-01-05 20:11:16 +0100

    images

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.BIRDS({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color2: 0xff00f0,
+        wingSpan: 40.00,
+        separation: 100.00,
+        alignment: 77.00,
+        cohesion: 5.00,
+        quantity: 2.00,
+        backgroundAlpha: 0.0
+      })
+})
